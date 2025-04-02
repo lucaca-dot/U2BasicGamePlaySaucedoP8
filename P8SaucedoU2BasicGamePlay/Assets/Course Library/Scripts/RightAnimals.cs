220aa9a3beabdeea8b707d80945075cff8aada89
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-04-02 15:27:32 -0700

    Added animals  coming from the right
    Animals can now come from the right.

---
+++ P8SaucedoU2BasicGamePlay/Assets/Course Library/Scripts/RightAnimals.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightAnimals : MonoBehaviour
+{ 
+
+
+    public GameObject[] animalPrefab1;
+private float spawnRangeZ = -15;
+private float spawnPosX = -25;
+private float startDelay = 2;
+private float spawnInterval = 1f;
+
+// Start is called before the first frame update
+void Start()
+{
+    InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+}
+
+void SpawnRandomAnimal()
+{
+    int animalIndex = Random.Range(0, animalPrefab1.Length);
+    Vector3 spawnPos = new Vector3(spawnPosX, 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+    Instantiate(animalPrefab1[animalIndex], spawnPos,
+    animalPrefab1[animalIndex].transform.rotation);
+}
+
+}
