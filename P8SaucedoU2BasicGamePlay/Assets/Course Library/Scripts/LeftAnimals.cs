3f6d3b26f2c94e323afc75aa2f17d9223b9a2a7b
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-04-04 15:28:44 -0700

    I finished
    I funishhed U2 and strated Challenge 2

---
+++ P8SaucedoU2BasicGamePlay/Assets/Course Library/Scripts/LeftAnimals.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftAnimals : MonoBehaviour
+{
+    public GameObject[] animalPrefab1;
+    private float spawnRangeZ = 30;
+    private float spawnPosX = 25;
+    private float startDelay = 2;
+    private float spawnInterval = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefab1.Length);
+        Vector3 spawnPos = new Vector3(-spawnPosX, 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+        Instantiate(animalPrefab1[animalIndex], spawnPos,
+        animalPrefab1[animalIndex].transform.rotation);
+    }
+
+}
+
+
